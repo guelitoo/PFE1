98018b16eb2022ca169b925428585d0ea660985e
guelitoo <159180695+guelitoo@users.noreply.github.com>
2025-05-20 12:59:30 -0300

    Update ex1-2.js

--- Exercícios de Funções 2/ex1-2.js
+++ Exercícios de Funções 2/ex1-2.js
@@ -3,6 +3,10 @@ function validateNumber(n) {
   throw new Error('Não é um número');
 }
 
+console.log(validateNumber(10));
+
 function validateUser(name, age) {
   return typeof name === 'string' && name !== '' && typeof age === 'numero' && age >= 0;
 }
+
+console.log(validateUser(Miguel, 18));
